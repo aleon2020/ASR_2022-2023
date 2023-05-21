d31dcd5d6785098e473ae3a55e7e17dc28b7041a
Alberto Leon Luengo <a.leon.2020@alumnos.urjc.es>
2023-05-21 13:50:42 +0200

    UPLOADING FILES ...

--- Paquetes Tema 1/my_package/src/simple.cpp
+++ Paquetes Tema 1/my_package/src/simple.cpp
@@ -1,8 +1,10 @@
 // Permite el acceso a la mayoría de tipos y funciones de C++.
 #include "rclcpp/rclcpp.hpp"
 int main(int argc, char * argv[]){
+
 	// Extrae los argumentos con los que el proceso se ejecutó.
 	rclcpp::init(argc,argv);
+	
 	// Creación de un nodo en ROS2 llamado simple_node.
 	// Tipo del nodo: std::shared_ptr (alias).
 	// make_shared: Método estático.
@@ -12,9 +14,12 @@ int main(int argc, char * argv[]){
 	// 3. rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node> (“simple_node”);
 	// 4. auto node = std::make_shared("simple_node");
 	auto node = rclcpp::Node::make_shared("simple_node");
+	
 	// Bloquea la ejecución del programa para que el programa no termine.
 	rclcpp::spin(node);
+	
 	// Gestiona el cierre de un nodo antes de acabar el programa.
 	rclcpp::shutdown();
+	
 	return 0;
 }
